ceebc2ba448f0de7c6c997b89019328f3b87575b
Jim Jacob <jim.jacob@tcs.com>
2021-05-22 18:27:48 +0100

    Initial Commit

---
+++ src/main/java/com/example/jjacob/OauthKeycloakApplication.java
@@ -0,0 +1,78 @@
+package com.example.jjacob;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootApplication
+@Configuration
+@EnableWebSecurity
+@RestController
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+public class OauthKeycloakApplication extends KeycloakWebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    public void configureGlobal(
+            AuthenticationManagerBuilder auth) throws Exception {
+
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider
+                = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
+                new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(
+                new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/admin*")
+                .hasRole("user")
+                .anyRequest()
+                .permitAll();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthKeycloakApplication.class, args);
+    }
+
+
+    @GetMapping("/admin")
+    public String user(Principal principal) {
+        return "Welcome, " + principal.getName();
+    }
+
+}
---
+++ src/test/java/com/example/jjacob/OauthKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jjacob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
